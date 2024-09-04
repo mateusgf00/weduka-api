da97481420c91f31035464a5fc6b78e447d4d985
Mateus Ferreira <mateusgf00@gmail.com>
2024-09-04 19:03:31 -0300

    adicionando porta

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://*:{port}");
+
 // Add services to the container.
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
